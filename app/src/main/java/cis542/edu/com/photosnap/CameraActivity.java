142c1efafb889fc04b5b7c068f0fd34ae2286612
David Mally <djmally@gmail.com>
2014-12-06 23:06:20 -0500

    Updated app so that it shows a camera preview in the app. Super unstable right now and also the x and y axes are flipped for some reason

--- app/src/main/java/cis542/edu/com/photosnap/CameraActivity.java
+++ app/src/main/java/cis542/edu/com/photosnap/CameraActivity.java
@@ -3,21 +3,30 @@ package cis542.edu.com.photosnap;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,7 +35,8 @@ import java.util.UUID;
 
 public class CameraActivity extends Activity {
 
-    private ImageView imageView;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String TAG = "CameraActivity";
 
     /* PEBBLE DATA CONSTANTS */
     private static final UUID PEBBLE_APP_UUID = UUID.fromString("3fb49826-013e-45d2-baaf-f10a2735556c");
@@ -41,25 +51,44 @@ public class CameraActivity extends Activity {
     private PebbleKit.PebbleDataReceiver dataHandler;
     /* PEBBLE VARIABLES */
 
-
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
-    private static final String TAG = "CameraActivity";
-
+    private View v;
+    private CameraPreview cameraPreview;
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
 
     /* BEGIN PRE-GENERATED CODE */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_camera);
 
-        this.imageView = (ImageView) findViewById(R.id.imageView);
-        Button capturebutton = (Button) findViewById(R.id.button_capture);
+        //this.imageView = (ImageView) findViewById(R.id.imageView);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //setContentView(R.layout.main);
+        mSurfaceView = (SurfaceView) findViewById(R.id.sv);
+        cameraPreview = new CameraPreview(this, mSurfaceView);
+        cameraPreview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        ((FrameLayout) findViewById(R.id.layout)).addView(cameraPreview);
+        cameraPreview.setKeepScreenOn(true);
+
+        cameraPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(shutterCallback, rawCallback, jpegCallback);
+            }
+        });
+
+        Toast.makeText(this, "Took a photo", Toast.LENGTH_LONG).show();
+        /*Button capturebutton = (Button) findViewById(R.id.button_capture);
         capturebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 distpatchPictureIntent();
             }
-        });
+        });*/
 
     }
 
@@ -91,10 +120,22 @@ public class CameraActivity extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-        // Start watch app
-        PebbleKit.startAppOnPebble(getApplicationContext(), PEBBLE_APP_UUID);
 
-        dataHandler = new PebbleKit.PebbleDataReceiver(PEBBLE_APP_UUID) {
+        int numCams = Camera.getNumberOfCameras();
+        if(numCams > 0) {
+            try {
+                mCamera = Camera.open(0);
+                mCamera.startPreview();
+                cameraPreview.setCamera(mCamera);
+            } catch (RuntimeException ex) {
+                Toast.makeText(this, "No camera found...", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        /* PEBBLE SECTION */
+        // Start Pebble app
+        PebbleKit.startAppOnPebble(getApplicationContext(), PEBBLE_APP_UUID);
+        this.dataHandler = new PebbleKit.PebbleDataReceiver(PEBBLE_APP_UUID) {
             @Override
             public void receiveData(Context context, int i, PebbleDictionary pebbleTuples) {
                 // ACK to prevent timeout
@@ -104,7 +145,7 @@ public class CameraActivity extends Activity {
                 int buttonPressed = pebbleTuples.getInteger(DATA_KEY).intValue();
 
                 // Take action
-                switch(buttonPressed) {
+                switch (buttonPressed) {
                     case SELECT_BUTTON_KEY:
                         break;
                     case UP_BUTTON_KEY:
@@ -116,28 +157,101 @@ public class CameraActivity extends Activity {
                 }
             }
         };
+        /* END PEBBLE SECTION */
 
+        // Register data handler
         PebbleKit.registerReceivedDataHandler(getApplicationContext(), dataHandler);
     }
 
     @Override
     public void onPause() {
-        super.onPause();
+        if(mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
 
+        /* PEBBLE SECTION */
         // Unregister Activity-scoped BroadcastReceivers when Activity is paused
         if(dataHandler != null) {
             unregisterReceiver(dataHandler);
             dataHandler = null;
         }
+        /* END PEBBLE SECTION */
+        super.onPause();
+
     }
 
+    private void resetCamera() {
+        mCamera.startPreview();
+        cameraPreview.setCamera(mCamera);
+    }
 
-    private void distpatchPictureIntent() {
+    private void refreshGallery(File file) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(Uri.fromFile(file));
+        sendBroadcast(mediaScanIntent);
+    }
+
+    private Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
+        @Override
+        public void onShutter() {
+            Log.d(TAG, "onShutter");
+        }
+    };
+
+    private Camera.PictureCallback rawCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.d(TAG, "onPictureTaken - raw");
+        }
+    };
+
+    private Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            new SaveImageTask().execute(data);
+            resetCamera();
+            Log.d(TAG, "onPictureTaken - jpeg");
+        }
+    };
+
+    private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+        @Override
+        protected Void doInBackground(byte[]... data) {
+            FileOutputStream outputStream = null;
+
+            try {
+                File sdCard = Environment.getExternalStorageDirectory();
+                File directory = new File(sdCard.getAbsolutePath() + "/photosnap");
+                directory.mkdirs();
+
+                String fileName = String.format("%d.jpg", System.currentTimeMillis());
+                File outFile = new File(directory, fileName);
+
+                outputStream = new FileOutputStream(outFile);
+                outputStream.write(data[0]);
+                outputStream.flush();
+                outputStream.close();
+
+                Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length + " to " +
+                            outFile.getAbsolutePath());
+                refreshGallery(outFile);
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    /*private void distpatchPictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-    }
+    }*/
 
     private File createImageFile() throws IOException {
         String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -155,12 +269,12 @@ public class CameraActivity extends Activity {
 
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
         }
-    }
+    }*/
 
     private void sendStringToPebble(String message) {
         if(message.length() > BUFFER_LENGTH) {
--- app/src/main/java/cis542/edu/com/photosnap/CameraPreview.java
+++ app/src/main/java/cis542/edu/com/photosnap/CameraPreview.java
@@ -16,115 +16,141 @@ import java.util.List;
  * Created by David on 12/6/2014.
  */
 public class CameraPreview extends ViewGroup implements SurfaceHolder.Callback {
-    private SurfaceView mSurfaceView;
-    private SurfaceHolder mHolder;
-    private Camera mCamera;
-    private List<Camera.Size> mSupportedPreviewSizes;
+    private final String TAG = "CameraPreview";
 
-    private static final String TAG = "CameraPreview";
+    SurfaceView mSurfaceView;
+    SurfaceHolder mHolder;
+    Camera.Size mPreviewSize;
+    List<Camera.Size> mSupportedPreviewSizes;
+    Camera mCamera;
 
-    private static final int K_STATE_PREVIEW = 0;
-    private static final int K_STATE_BUSY    = 1;
-    private static final int K_STATE_FROZEN  = 2;
-
-    CameraPreview(Context context, Camera camera) {
+    CameraPreview(Context context, SurfaceView sv) {
         super(context);
-        mCamera = camera;
 
-        mSurfaceView = new SurfaceView(context);
-        addView(mSurfaceView);
+        mSurfaceView = sv;
 
-        // Install a SurfaceHolder.Callback so we get notified
-        // when the underlying surface is created and destroyed.
         mHolder = mSurfaceView.getHolder();
         mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
     public void setCamera(Camera camera) {
-        if(mCamera == camera) { return; }
-
-        stopPreviewAndFreeCamera();
-
-        mCamera = camera;
-
+        this.mCamera = camera;
         if(mCamera != null) {
             mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
             requestLayout();
 
-            try {
-                mCamera.setPreviewDisplay(mHolder);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            Camera.Parameters params = mCamera.getParameters();
 
-            mCamera.startPreview();
+            List<String> focusModes = params.getSupportedFocusModes();
+            if(focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                params.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+                mCamera.setParameters(params);
+            }
         }
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+        setMeasuredDimension(width, height);
+
+        if(mSupportedPreviewSizes != null) {
+            mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
+        }
+    }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if(changed && getChildCount() > 0) {
+            final View child = getChildAt(0);
 
+            final int width = r - l;
+            final int height = b - t;
+
+            int previewWidth = width;
+            int previewHeight = height;
+            if(mPreviewSize != null) {
+                previewWidth = mPreviewSize.width;
+                previewHeight = mPreviewSize.height;
+            }
+
+            // Center child SurfaceView within parent
+            if(width * previewHeight > height * previewWidth) {
+                final int scaledChildWidth = previewWidth * height / previewHeight;
+                child.layout((width - scaledChildWidth) / 2, 0,
+                        (width + scaledChildWidth) / 2, height);
+            } else {
+                final int scaledChildHeight = previewHeight * width / previewWidth;
+                child.layout(0, (height - scaledChildHeight) / 2,
+                        width, (height + scaledChildHeight) / 2);
+            }
+        }
     }
 
-    @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        // Surface has been created, tell camera where to draw preview
+        // Surface created, acquire camera and draw
         try {
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
+            if(mCamera != null) {
+                mCamera.setPreviewDisplay(holder);
+            }
         } catch (IOException ex) {
-            Log.d(TAG, "Error setting camera preview: " + ex.getMessage());
+            Log.e(TAG, "IOException caused by setPreviewDisplay()", ex);
         }
     }
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        if(mHolder.getSurface() == null) {
-            // Preview surface does not exist
-            return;
-        }
-
-        try {
-            mCamera.stopPreview();
-        } catch (Exception ex) {
-            Log.d(TAG, "Tried to stop nonexistent preview");
-        }
-
-        // Set preview size and make any resize, rotate, or reformatting changes
-        // (Optional)
-
-        // Start preview with new settings
-        try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
-        } catch (Exception ex) {
-            Log.d(TAG, "Error starting camera preview: " + ex.getMessage());
-        }
-    }
-
-    @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        // Surface will be destroyed when we return, so stop preview
+        // Surface will be destroyed when we return, so stop previewing
         if(mCamera != null) {
-            // Call stopPreview() to stop updating preview surface
             mCamera.stopPreview();
         }
     }
 
-    // When this function returns, mCamera will be null
-    private void stopPreviewAndFreeCamera() {
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) w / h;
+        if(sizes == null) { return null; }
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for(Camera.Size sz : sizes) {
+            double ratio = (double) sz.width / sz.height;
+            if(Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) { continue; }
+            if(Math.abs(sz.height - targetHeight) < minDiff) {
+                optimalSize = sz;
+                minDiff = Math.abs(sz.height - targetHeight);
+            }
+        }
+
+        if(optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for(Camera.Size sz : sizes) {
+                if(Math.abs(sz.height - targetHeight) < minDiff) {
+                    optimalSize = sz;
+                    minDiff = Math.abs(sz.height = targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         if(mCamera != null) {
-            // Stop updating preview surface
-            mCamera.stopPreview();
+            Camera.Parameters params = mCamera.getParameters();
+            List<Camera.Size> previewSizes = params.getSupportedPreviewSizes();
 
-            // Stop using camera so that other applications can use it.
-            // Should be released immediately during onPause() and re-open()
-            // on onResume()
-            mCamera.release();
+            int previewWidth = previewSizes.get(0).width;
+            int previewHeight = previewSizes.get(0).height;
 
-            mCamera = null;
+            params.setPreviewSize(previewWidth, previewHeight);
+            requestLayout();
+
+            mCamera.setParameters(params);
+            mCamera.startPreview();
         }
     }
 }
